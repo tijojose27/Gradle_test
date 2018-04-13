dbaecce6f42cb5b88a9a860fba04f0292611a8f1
tijojose27 <tijojose27@yahoo.com>
2018-04-13 15:49:29 -0500

    feat : add JavaJokesLib
    JavaJokes Library added to the project and jokes are pulled from it. The gradle file has been updated too to implement this class

---
+++ JavaJokesLib/src/main/java/com/example/javajokeslib/JavaJokes.java
@@ -0,0 +1,8 @@
+package com.example.javajokeslib;
+
+public class JavaJokes {
+    public static String getJoke(){
+        String joke =" A wife is like a hand grenade....remove the ring and your house is gone";
+        return joke;
+    }
+}
