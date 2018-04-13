28528c157a41e6986c2d91d945f719d5f698ebe8
tijojose27 <tijojose27@yahoo.com>
2018-04-13 16:01:52 -0500

    feat : add androidjokeslib
    androidjokeslib Library added to the project and new intent created to call this activity once the button is pressed. it displays the joke from the javajokeslib

---
+++ androidjokeslib/src/main/java/com/example/androidjokeslib/JokeActivity.java
@@ -0,0 +1,19 @@
+package com.example.androidjokeslib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.javajokeslib.JavaJokes;
+
+public class JokeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(JavaJokes.getJoke());
+    }
+}
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -7,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.androidjokeslib.JokeActivity;
 import com.example.javajokeslib.JavaJokes;
 
 
@@ -43,6 +45,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void tellJoke(View view) {
         Toast.makeText(this, JavaJokes.getJoke() , Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, JokeActivity.class);
+
+        startActivity(intent);
     }
 
 
